6de97d81ab4a4c1b8e66e17d6c1a76746b4599a9
bernabasa <75853223+bernabasa@users.noreply.github.com>
2022-10-29 16:47:33 -0400

    corner stone planted

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var Questions = [
+    {
+        Title: "Who is the first presedant of The United States of America?",
+        Choices: ["Barck Obama",
+                "Clinton",
+                "George Washington",
+                "Thomas Jefferson"],
+        Answer: "George Washington"
+    },
+    {
+        Title: "How many amendments does the Constitution have?",
+        Choices:["30",
+                "27",
+                "58",
+                "74"],
+        Answer: "27"
+    },
+    {
+        Title: "How many U.S. Senators are there?",
+        Choices: ["50",
+                "21",
+                "40",
+                "100"],
+
+        Answer: "100"
+    },
+];
+var Top = document.querySelector("#Top");
+var Timer = document.querySelector("#Timer");
+var Quest = document.querySelector("#quest");
+var QuestInfo = document.querySelector("#QuestInfo");
+var StartButton = document.querySelector("#startButton");
